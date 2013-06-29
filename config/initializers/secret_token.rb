ac1d8d8833c5c146da1f6fcf75d8cac4b52bdd7e
Eddy Kim <eddyhkim@gmail.com>
2013-06-29 09:50:22 -0700

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tribalknow::Application.config.secret_key_base = 'a05b3986eb3cb1c33a8cc0bb57d474091ccbdab748098e5c03a60ca0f295371ee08ac8d5db050df317ea3d7de86785102eedbf3b16da71ac85abbeccc9181510'
