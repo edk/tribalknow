8d480f58034b8cfeba9f684ac4fb6a1506a371fb
Eddy Kim <eddyhkim@gmail.com>
2013-07-23 19:41:18 -0700

    move to database backed sessions

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -7,6 +7,6 @@ class ApplicationController < ActionController::Base
 
   private
   def current_user
-    @current_user ||= User.find(session[:user_id]) if session[:user_id]
+    @current_user ||= User.find_by_user_id(session[:user_id]) if session[:user_id]
   end
 end
--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -1,3 +1,5 @@
 # Be sure to restart your server when you modify this file.
 
-Tribalknow::Application.config.session_store :cookie_store, key: '_tribalknow_session'
+#Tribalknow::Application.config.session_store :cookie_store, key: '_tribalknow_session'
+
+Tribalknow::Application.config.session_store :active_record_store
