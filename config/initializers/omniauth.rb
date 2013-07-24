65f31d48161141ade87a23cbae827e8228eafc32
Eddy Kim <eddyhkim@gmail.com>
2013-07-23 20:30:03 -0700

    add some feedback if you forget to set the github key and secret in the env

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,13 @@
+
+if !ENV['GITHUB_KEY'].present? || !ENV['GITHUB_SECRET'].present?
+  puts "*" * 80
+  puts "ENV['GITHUB_KEY'] is NOT SET."
+  puts "ENV['GITHUB_SECRET'] is NOT SET"
+  puts "*" * 80
+else
+  puts "GITHUB_SECRET and GITHUB_KEY set"
+end
+
 Rails.application.config.middleware.use OmniAuth::Builder do
   # provider :developer unless Rails.env.production?
   require 'openid/store/filesystem' 
