664ac7fb0ea444cab811e6ae53829caf439ac437
Eddy Kim <eddyhkim@gmail.com>
2013-10-05 18:32:45 -0700

    Adding solr

--- app/models/question.rb
+++ app/models/question.rb
@@ -7,4 +7,11 @@ class Question < ActiveRecord::Base
   has_many :answers
 
   validates  :title, :text, presence: true, length: { minimum: 3 }
+
+  searchable do
+    text :title, :text
+    text :tags do
+      tags.join(', ')
+    end
+  end
 end
