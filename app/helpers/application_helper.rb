60871a45e29a3cb41ade50ff3b9daf89f4b20f8b
Eddy Kim <eddyhkim@gmail.com>
2013-10-14 22:16:26 -0700

    get some styling for code blocks

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,7 +1,9 @@
 module ApplicationHelper
   def render_md(text)
     markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML.new(prettify:true, hard_wrap:true),
-        :autolink => true, :space_after_headers => true, :fenced_code_blocks=>true, :underline=>false, :footnotes=>true)
+        :autolink => true, :space_after_headers => true,
+        :fenced_code_blocks=>true, :disable_indented_code_blocks=>true, :underline=>true, :footnotes=>true,
+        :no_intra_emphasis=>true, :tables=>true)
     markdown.render(text).html_safe
   end
 
