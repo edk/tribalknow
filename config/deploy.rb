f2ed6732c195f1db2404a8cf62a377ee16dfea2c
Eddy Kim <eddyhkim@gmail.com>
2013-12-14 16:29:37 -0800

    git ignore solr data dir updating linked files in deploy update linked dir and there's no cache clear task

--- config/deploy.rb
+++ config/deploy.rb
@@ -10,12 +10,12 @@ set :branch, 'master'
 set :deploy_to, '/home/deploy/tribalknow'
 set :scm, :git
 
-#set :format, :pretty
+set :format, :pretty
 set :log_level, :debug
 # set :pty, true
 
-set :linked_files, %w{config/database.yml}
-set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
+set :linked_files, %w{config/database.yml config/initializers/secret_token.rb}
+set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system solr/data}
 
 # set :default_env, { path: "/opt/ruby/bin:$PATH" }
 set :keep_releases, 5
@@ -25,19 +25,17 @@ namespace :deploy do
   desc 'Restart application'
   task :restart do
     on roles(:app), in: :sequence, wait: 5 do
-      # Your restart mechanism here, for example:
-      # execute :touch, release_path.join('tmp/restart.txt')
+      execute :touch, release_path.join('tmp/restart.txt')
     end
   end
 
-  after :restart, :clear_cache do
-    on roles(:web), in: :groups, limit: 3, wait: 10 do
-      # Here we can do anything such as:
-      # within release_path do
-      #   execute :rake, 'cache:clear'
-      # end
-    end
-  end
+  # after :restart, :clear_cache do
+  #   on roles(:web), in: :groups, limit: 3, wait: 10 do
+  #     # within release_path do
+  #     #   execute :rake, 'cache:clear'
+  #     # end
+  #   end
+  # end
 
   desc "uptime"
   task :uptime do |host|
