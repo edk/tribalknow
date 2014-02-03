77d52703f4bf8cd7be7e7e67905b489f315573f7
Eddy Kim <eddyhkim@gmail.com>
2014-02-03 06:35:08 -0800

    update schema

--- db/schema.rb
+++ db/schema.rb
@@ -142,5 +142,6 @@ ActiveRecord::Schema.define(version: 20140125031820) do
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
   add_index "users", ["provider", "uid"], name: "index_users_on_provider_and_uid", unique: true, using: :btree
   add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
+  add_index "users", ["tenant_id"], name: "index_users_on_tenant_id", using: :btree
 
 end
