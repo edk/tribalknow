950bf3914270ff4ae3a75b648adf40af8d48bdaa
Eddy Kim <eddyhkim@gmail.com>
2014-03-23 12:08:07 -0700

    remove old code

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,11 +1,6 @@
 
 module ApplicationHelper
   def render_md(text)
-    # markdown = Redcarpet::Markdown.new(Redcarpet::Render::HTML.new(prettify:true, hard_wrap:true),
-    #     :autolink => true, :space_after_headers => true,
-    #     :fenced_code_blocks=>true, :disable_indented_code_blocks=>true, :underline=>true, :footnotes=>true,
-    #     :no_intra_emphasis=>true, :tables=>true)
-    # markdown.render(text).html_safe
     GitHub::Markdown.render_gfm(sanitize(text)).html_safe
   end
 
