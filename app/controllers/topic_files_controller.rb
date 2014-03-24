679ff056b97b62b0f97e443790c1fc3a1c7351b2
Eddy Kim <eddyhkim@gmail.com>
2014-03-24 16:49:51 -0700

    fix crash and other things wip

--- app/controllers/topic_files_controller.rb
+++ app/controllers/topic_files_controller.rb
@@ -10,10 +10,10 @@ class TopicFilesController < ApplicationController
     @topic = Topic.friendly.find(params[:topic_id])
     asset = @topic.topic_files.create :asset=>params[:file]
     respond_to do |format|
-      format.json { render :json=>asset}
+      format.json { render :json=>asset.as_json.merge(:thumb_url=>asset.asset.url(:thumb), :url=>asset.asset.url) }
     end
   end
-  
+
   def destroy
     
   end
