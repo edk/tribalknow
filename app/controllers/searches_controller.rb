4236b8525157359907a134a888167a1fea44bfc0
Eddy Kim <eddyhkim@gmail.com>
2014-04-03 00:05:08 -0700

    cleanup search results page, add excerpts to some of the result types

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -3,7 +3,9 @@ class SearchesController < ApplicationController
 
   def index
     @results = ThinkingSphinx.search params[:q],
+                                    :excerpts => { :limit=>255, :around=>20 },
                                     :classes => [Question, Answer, Topic],
                                     :with=>{ :tenant_id => Tenant.current_id }
+    @results.context[:panes] << ThinkingSphinx::Panes::ExcerptsPane
   end
 end
