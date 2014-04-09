feb5b613cbe9163326bf8fc55bb8323dc0bbdd25
Eddy Kim <eddyhkim@gmail.com>
2014-04-09 09:34:11 -0700

    make question list clickable

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -1,6 +1,12 @@
 # Be sure to restart your server when you modify this file.
 
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-Tribalknow::Application.config.secret_key_base = 'a05b3986eb3cb1c33a8cc0bb57d474091ccbdab748098e5c03a60ca0f295371ee08ac8d5db050df317ea3d7de86785102eedbf3b16da71ac85abbeccc9181510'
+Tribalknow::Application.config.secret_key_base = '70f0b6840ebe1f053b3967e175e5b7cfc693fe5f217333741ad87e93c51f8a53e4faa238278acae39b4e43ce06405d61ccc1661961a4195a2c14c799e42765ea'
