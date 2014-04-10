74dd5f492d598cf187d4a387ff5586bd0c930388
Eddy Kim <eddyhkim@gmail.com>
2014-04-10 12:18:01 -0700

    reject without send email, and add subdomain to the admin mailer

--- app/controllers/approve_users_controller.rb
+++ app/controllers/approve_users_controller.rb
@@ -16,7 +16,7 @@ class ApproveUsersController < ApplicationController
     user = User.find(params[:id]) # default scope shoudl still restrict to current tenant
     authorize user, :update?
     if user # actually find should throw if not found.
-      AdminMailer.user_account_rejected(user).deliver
+      AdminMailer.user_account_rejected(user).deliver unless params[:skip_notification]
       user.destroy!
     end
     flash[:notice] = "Successfully rejected and deleted user account."
