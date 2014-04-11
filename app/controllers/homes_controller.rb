8978bb7030372e4a0121e20fe3614fac8e916e02
Eddy Kim <eddyhkim@gmail.com>
2014-04-10 23:01:30 -0700

    some cleanup of the activity feed.  will need more work once more live data is generated

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -4,7 +4,7 @@ class HomesController < ApplicationController
   def index
     if current_user && Tenant.current_id
       # @activities = PublicActivity::Activity.all
-      @activities = PublicActivity::Activity.order("created_at DESC").where('created_at > ?', 20.days.ago).limit(100)
+      @activities = PublicActivity::Activity.order("created_at DESC").where('created_at > ?', 14.days.ago).limit(50)
       @collapsed_activities = @activities.inject({}) { |m,o| k="#{o.trackable_type}:#{o.trackable_id}"; m[k] ||= []; m[k] << {:key=>o.key,:at=>o.created_at,:owner=>o.owner, :obj=>o}  ;m }
     else
       render :action => 'public_index'
