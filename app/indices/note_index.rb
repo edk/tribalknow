41fafe3c155012b78a641960a397f531ff710719
Eddy Kim <eddyhkim@gmail.com>
2014-08-11 00:20:39 -0700

    ignoring tenant_id for now

--- app/indices/note_index.rb
+++ app/indices/note_index.rb
@@ -6,5 +6,5 @@ ThinkingSphinx::Index.define :note, :with => :active_record do
   indexes creator.name, :as=>:creator_name, :sortable => true
 
   # attributes
-  has tenant_id, creator_id, updater_id, created_at, updated_at
+  has creator_id, updater_id, created_at, updated_at
 end
