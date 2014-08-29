3dd5d7225d67652a93865ce4ab130fb4cc8510ec
Dmitri Korobov <dimitrikorobov@DimitriKorobovs-MacBook-Pro.local>
2014-08-28 17:14:15 -0700

    autocomplete - fixing answer results

--- app/controllers/searches_controller.rb
+++ app/controllers/searches_controller.rb
@@ -7,7 +7,7 @@ class SearchesController < ApplicationController
     respond_to do |format|
       format.html #do nothing, defaults to default template
       format.json do
-        @results.map!{|result| { label: result.title, category: result.class.model_name.human, url: url_for(result) }}
+        @results.map!{|result| { label: result.title, category: result.class.model_name.human, url: resource_url(result) }}
         render :json => @results.to_a
       end
     end
@@ -19,6 +19,11 @@ class SearchesController < ApplicationController
                           :excerpts => { :limit=>255, :around=>20 },
                           :classes => [Question, Answer, Topic, Note],
                           :with=>{ :tenant_id => Tenant.current_id }
+
+  end
+
+  def resource_url(resource)
+    resource.instance_of?(Answer) ? url_for(resource.question) : url_for(resource)
 
   end
 end
