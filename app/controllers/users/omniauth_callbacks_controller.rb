7fec58d4c5ab2f703466215939838a7e97e3dd79
Eddy Kim <eddyhkim@gmail.com>
2014-08-28 18:20:19 -0700

    remove debug log, add user:email scope.  proably won't help though. sigh

--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -1,7 +1,6 @@
 class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
   def github
     user = User.from_omniauth(request.env["omniauth.auth"])
-    Rails.logger.error("XXX debug only: request.env['omniauth.auth'] = #{request.env["omniauth.auth"].inspect})"
     if user.persisted?
       sign_in_and_redirect user, notice: 'signed in!'
     else
