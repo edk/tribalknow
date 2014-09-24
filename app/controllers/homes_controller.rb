a0c0a82b2bf025a62990358c264b188a924e49bd
farrspace <brian.farr@gmail.com>
2014-09-23 17:36:33 -0700

    Popular questions - implement a simple ranking by positive vote count

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -6,6 +6,8 @@ class HomesController < ApplicationController
       # @activities = PublicActivity::Activity.all
       @activities = PublicActivity::Activity.order("created_at DESC").where('created_at > ?', 14.days.ago).limit(50)
       @collapsed_activities = @activities.inject({}) { |m,o| k="#{o.trackable_type}:#{o.trackable_id}"; m[k] ||= []; m[k] << {:key=>o.key,:at=>o.created_at,:owner=>o.owner, :obj=>o}  ;m }
+
+      @popular_questions = Question.popular
     else
       if (landing_page = Tenant.current.try(:landing_page)).present?
         render :text => landing_page, :layout=>true
