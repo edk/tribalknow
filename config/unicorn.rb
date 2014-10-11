39e1e940fce913a54e378bdefe5f0bab5fa480b9
Eddy Kim <eddyhkim@gmail.com>
2014-10-11 09:58:09 -0700

    Initial experiment with unicorn

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -6,3 +6,22 @@ else
 end
 
 timeout 30
+
+# Set the working application directory
+# working_directory "/path/to/your/app"
+#working_directory "/var/www/my_app"
+
+# Unicorn PID file location
+# pid "/path/to/pids/unicorn.pid"
+#pid "/var/www/my_app/pids/unicorn.pid"
+
+# Path to logs
+# stderr_path "/path/to/log/unicorn.log"
+# stdout_path "/path/to/log/unicorn.log"
+#stderr_path "/var/www/my_app/log/unicorn.log"
+#stdout_path "/var/www/my_app/log/unicorn.log"
+
+# Unicorn socket
+#listen "/tmp/unicorn.[app name].sock"
+#listen "/tmp/unicorn.myapp.sock"
+
