ee5c6ad040a657ffba949ac9416cedd8dafdb55b
Eddy Kim <eddyhkim@gmail.com>
2015-03-02 23:12:35 -0800

    reverse the initial display order.  temporary until this gets rewritten

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,6 +1,6 @@
 class Admin::UsersController < Admin::HomeController
   def index
-    @users = User.paginate(:page=>params[:page])
+    @users = User.order('id desc').paginate(:page=>params[:page])
     authorize @users
   end
 end
