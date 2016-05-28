70a6cb54669a73da65f7391247a6ddcf85d25a94
Eddy Kim <eddyhkim@gmail.com>
2016-05-28 14:19:24 -0700

    bump up the recent activity panel

--- app/controllers/homes_controller.rb
+++ app/controllers/homes_controller.rb
@@ -4,7 +4,7 @@ class HomesController < ApplicationController
   def index
     if current_user && Tenant.current_id
       # @activities = PublicActivity::Activity.all
-      @activities = PublicActivity::Activity.order("created_at DESC").where('created_at > ?', 14.days.ago).limit(50)
+      @activities = PublicActivity::Activity.order("created_at DESC").where('created_at > ?', 30.days.ago).limit(50)
       @collapsed_activities = @activities.inject({}) do |m,o|
         k = "#{o.trackable_type}:#{o.trackable_id}"
         m[k] ||= []
