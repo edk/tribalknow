80d1981f40531150c2d433e0245bbe489f9aa3e1
Eddy Kim <eddyhkim@gmail.com>
2016-06-02 16:34:51 -0700

    quick fix to increase page display, was missing the pagination links.  need it now

--- app/controllers/topics_controller.rb
+++ app/controllers/topics_controller.rb
@@ -2,7 +2,7 @@ class TopicsController < ApplicationController
 
   def index
     # using the .roots scope orders by lft which is not what we want on the index.
-    @topics = Topic.order(:name).where(:parent_topic_id=>nil).paginate(:page=>params[:page], :per_page=> 3*9 )  # 3 per row, 9 rows
+    @topics = Topic.order(:name).where(:parent_topic_id=>nil).paginate(:page=>params[:page], :per_page=> 3*20 )  # 3 per row, 9 rows
   end
 
   def show
