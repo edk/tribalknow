0eab5a4c49b8d3c3b8d10e8856c229e01e441abf
Eddy Kim <eddyhkim@gmail.com>
2016-07-10 08:55:39 -0700

    Some analytics.  charts and stuff

--- app/controllers/admin/analytics_controller.rb
+++ app/controllers/admin/analytics_controller.rb
@@ -1,4 +1,12 @@
 
 class Admin::AnalyticsController < ApplicationController
+  def index
+    @stats = []
+    @stats << ['User signups by week', User.group_by_week(:created_at).count, :column_chart]
+    @stats << ['Usage by country', Visit.group(:country).count, :geo_chart]
+    @stats << ['Top Searches',
+               Ahoy::Event.where(name: 'searches#index').group(:properties).count.inject({}) {|m, (k,v)| m[k["q"]] = v ; m  },
+               :pie_chart]
+  end
 end
 
