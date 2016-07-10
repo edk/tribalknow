eca9c450a32d257191f9d99d2d0704d632a5f840
Eddy Kim <eddyhkim@gmail.com>
2016-07-10 13:42:37 -0700

    Track logins too

--- app/controllers/admin/analytics_controller.rb
+++ app/controllers/admin/analytics_controller.rb
@@ -2,11 +2,18 @@
 class Admin::AnalyticsController < ApplicationController
   def index
     @stats = []
-    @stats << ['User signups by week', User.group_by_week(:created_at).count, :column_chart]
+    @stats << ['User signups by week', User.group_by_week(:created_at).count, :column_chart, {label: 'Number of signups'}]
+    @stats << ['User Logins',
+                [
+                  { name: 'Manual login', data: Ahoy::Event.where(name: 'sessions#create').group_by_day(:time).count },
+                  { name: 'Oauth login', data: Ahoy::Event.where(name: 'omniauth-login-ok').group_by_day(:time).count },
+                ],
+               :line_chart, { stacked: true } ]
+    puts @stats.last.inspect
     @stats << ['Usage by country', Visit.group(:country).count, :geo_chart]
     @stats << ['Top Searches',
                Ahoy::Event.where(name: 'searches#index').group(:properties).count.inject({}) {|m, (k,v)| m[k["q"]] = v ; m  },
-               :pie_chart]
+               :pie_chart, {label: 'Search Term'} ]
   end
 end
 
--- app/controllers/users/omniauth_callbacks_controller.rb
+++ app/controllers/users/omniauth_callbacks_controller.rb
@@ -4,8 +4,10 @@ class Users::OmniauthCallbacksController < Devise::OmniauthCallbacksController
     
     if user && !user.active?
       Rails.logger.error("DBG omniauth_callbacks_controller.rb: #{__LINE__} !user.active?")
+      ahoy.track("omniauth-login-failed", {id: user.id, name: user.name, email: user.email, provider: user.provider, errors: user.errors.full_messages.join(', ')})
       redirect_to new_user_session_path, notice: user.errors.full_messages.join("<br/>").html_safe
     elsif user && user.persisted?
+      ahoy.track("omniauth-login-ok", {id: user.id, name: user.name, email: user.email, provider: user.provider})
       sign_in_and_redirect user, notice: 'signed in!'
     else
       if !user && session.has_key?(:from_omniauth)
