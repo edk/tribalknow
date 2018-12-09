cff809b100ae8de4238dc3f256bce636d5c37566
Eddy Kim <eddyhkim@gmail.com>
2018-12-09 13:06:06 -0800

    reformat

--- app/controllers/api/base_controller.rb
+++ app/controllers/api/base_controller.rb
@@ -6,11 +6,11 @@ class Api::BaseController < ApplicationController
 
   protected
 
-    def authenticate_api
-      if AppConfig[:api_key] != params[:api_key] || params[:api_key].blank?
-        render :text => "Sorry, you don't have access.", :status => 403
-        return
-      end
+  def authenticate_api
+    if AppConfig[:api_key] != params[:api_key] || params[:api_key].blank?
+      render :text => "Sorry, you don't have access.", :status => 403
+      return
     end
+  end
 
 end
