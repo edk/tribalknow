c841761d21855961f4752703a5649dc46af218d9
Eddy Kim <eddyhkim@gmail.com>
2019-05-14 13:16:01 -0700

    Updating to use bootstrap from npm and related bootstrap changes

--- config/webpack/production.js
+++ config/webpack/production.js
@@ -2,7 +2,4 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'production'
 
 const environment = require('./environment')
 
-// avoid sourcemaps from being generated in production.
-environment.config.merge({ devtool: "none" });
-
 module.exports = environment.toWebpackConfig()
