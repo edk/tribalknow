5b1a617f53562810bf90ff78391915ea8334b471
Eddy Kim <eddyhkim@gmail.com>
2020-05-24 21:29:57 -0700

    add jquery-ujs and some other minor touchups - yes, js needs major overhaul

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -13,6 +13,13 @@ environment.plugins.prepend('Provide',
   Popper: ['popper.js', 'default']
 }))
 
+// const aliasConfig = {
+//   'jquery': 'jquery-ui-dist/external/jquery/jquery.js',
+//   'jquery-ui': 'jquery-ui-dist/jquery-ui.js'
+// };
+
+// environment.config.set('resolve.alias', aliasConfig);
+
 // resolve-url-loader must be used before sass-loader
 // https://github.com/rails/webpacker/blob/master/docs/css.md#resolve-url-loader
 environment.loaders.get("sass").use.splice(-1, 0, {
