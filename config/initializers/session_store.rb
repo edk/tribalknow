6ac8fa05e4d4d2cfccf87a0b847e9fccc564817b
Eddy Kim <eddyhkim@gmail.com>
2020-05-25 15:52:59 -0700

    remove possible csrf breakage

--- config/initializers/session_store.rb
+++ config/initializers/session_store.rb
@@ -2,4 +2,4 @@
 
 #Tribalknow::Application.config.session_store :cookie_store, key: '_tribalknow_session'
 
-Tribalknow::Application.config.session_store :active_record_store, same_site: :lax, secure: Rails.env.production?
+Tribalknow::Application.config.session_store :active_record_store, secure: Rails.env.production?
