906794a492d1098ca72abfc86c4430e6e4afba16
Eddy Kim <eddyhkim@gmail.com>
2020-05-30 20:41:09 -0700

    First commit to update to rails 6 rake app:update

--- Gemfile
+++ Gemfile
@@ -3,7 +3,7 @@ git_source(:github) { |repo| "https://github.com/#{repo}.git" }
 
 ruby '2.6.5'
 
-gem 'rails', '~> 5.2'
+gem 'rails', '~> 6.0'
 gem 'puma'
 gem 'sass-rails', '~> 5.0'
 gem 'uglifier', '>= 1.3.0'
--- config/application.rb
+++ config/application.rb
@@ -3,16 +3,17 @@ require_relative 'boot'
 require "rails"
 # Pick the frameworks you want:
 require "active_model/railtie"
-# require "active_job/railtie"
+require "active_job/railtie"
 require "active_record/railtie"
 # require "active_storage/engine"
 require "action_controller/railtie"
 require "action_mailer/railtie"
+# require "action_mailbox/engine"
+# require "action_text/engine"
 require "action_view/railtie"
-# require "action_cable/engine"
-# require "sprockets/railtie"
+require "action_cable/engine"
+require "sprockets/railtie"
 require "rails/test_unit/railtie"
-require 'action_cable/engine'
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -23,15 +24,14 @@ module Tribalknow
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.0
 
-    config.autoload_paths << Rails.root.join("lib")
-    config.autoload_paths << Rails.root.join("app/channels")
-    config.eager_load_paths << Rails.root.join("lib")
-    config.eager_load_paths << Rails.root.join("app/channels")
-
-    config.cache_store = :redis_store, ENV['REDIS_STORE_URL'] || "redis://localhost:6379/0/cache" #, { expires_in: 90.minutes }
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.autoload_paths << Rails.root.join("lib")
+    config.autoload_paths << Rails.root.join("app/channels")
+    config.eager_load_paths << Rails.root.join("lib")
+    config.eager_load_paths << Rails.root.join("app/channels")
+    config.cache_store = :redis_store, ENV['REDIS_STORE_URL'] || "redis://localhost:6379/0/cache" #, { expires_in: 90.minutes }
   end
 end
