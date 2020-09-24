acf60f3d3c66bb79da818e6cc25fbb489af86854
Eddy Kim <eddyhkim@gmail.com>
2020-09-24 10:25:27 -0700

    get some tests passing

--- spec/models/site_stat_spec.rb
+++ spec/models/site_stat_spec.rb
@@ -2,12 +2,23 @@ require 'rails_helper'
 
 RSpec.describe SiteStat, type: :model do
   context "empty recent activity" do
+    before do
+      SiteStat.stub_chain(:where, :last).and_return([])
+    end
+
     it "returns none" do
       expect(SiteStat.recent_activity).to be_empty
     end
   end
 
   context "some recent activity" do
+    let(:tenant) { FactoryBot.create(:tenant) }
+    before do
+      Tenant.current_id = tenant.id
+    end
+    after do
+      Tenant.current_id = nil
+    end
     it "generates a new cached record" do
       SiteStat.generate_recent_activity!
     end
