6aafbac49c14005431b20eee2107b0bccc0a205d
Eddy Kim <eddyhkim@gmail.com>
2020-09-30 11:33:58 -0700

    Fix search. Add needed extensions and set options before search

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_24_200057) do
+ActiveRecord::Schema.define(version: 2020_09_30_184330) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "hstore"
+  enable_extension "pg_trgm"
   enable_extension "plpgsql"
+  enable_extension "unaccent"
 
   create_table "activities", force: :cascade do |t|
     t.bigint "trackable_id"
